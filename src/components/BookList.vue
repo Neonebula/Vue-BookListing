999ea151a113490c2032d281ce4f31b7eeb9515a
Bryan Boukari <bryanbkr@MacBook-Pro-de-Bryan.local>
2019-01-17 17:29:51 +0100

    Creating the BookList Component

---
+++ src/components/BookList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{title}}</h1>
+    </div>
+</template>
+<script>
+    export default{
+        name : "BookList",
+        title : "All Books",
+        data(){
+            return {
+                title : "All Books",
+            };
+        }
+    }
+</script>
+<style>
+    h1 h2{
+        font-weight: normal;
+    }
+</style>
+
+
